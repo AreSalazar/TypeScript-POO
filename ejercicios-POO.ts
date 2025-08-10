5d53e5f26b9037e40c3b7ad14921f502d81dc13b
AreSalazar <andrea.raymundosalazar@gmail.com>
2025-08-10 14:34:17 -0600

    Feat: Archivo creado

---
+++ ejercicios-POO.ts
@@ -0,0 +1,4 @@
+/*EJERCICIO 1. Crear una clase Cabecera Pagina, que contenga 3 métodos, el primer método que
+obtenga el título, color y fuente de la página, el segundo método que indique como desea que
+aparezca el título si centrado, derecha o izquierda y el tercer método que imprima todas las
+propiedades*/
